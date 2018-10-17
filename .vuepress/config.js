74b9135827be35cb2ae0927b1b98350abae6422e
choijaewoong <jwchoi0129@gmail.com>
2018-10-17 17:59:08 +0900

    feat:add navbar and sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,41 @@
 module.exports = {
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+        
+        // navbar: false,
+        nav: 
+        [
+            // NavMenu - DropdownMenu
+            {
+                text: 'Languages',
+                items: [
+                    { text: 'Chinese', link: '/' },
+                    { text: 'Japanese', link: '/'}
+                ]
+            },
+            // NavMenu - DefaultLink
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide' },
+            { text: 'GitHub', link: 'https://github.com/choijaewoong' }
+        ],
+
+        // sidebar: false,
+        sidebar: {
+            '/': [
+                ['/', 'First'],
+                ['/', 'Second'],
+                ['/', 'Third'],
+                {
+                    title: 'Group 2',
+                    collapsable: false,
+                    children: [
+                        ['/', 'First'],
+                        ['/', 'Second'],
+                        ['/', 'Third'],
+                    ]
+                }
+            ]
+        }
+    }
 }
